e80e9ff2b963b0fad24a96f44c0b4fd931f158f6
Fernanda de Jesus <fernandabussular@gmail.com>
2023-11-16 23:39:05 -0300

    add devise

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :email, uniqueness: true, presence: true
-  validates :pass, presence: true
+  validates :encrypted_password, presence: true
 end
