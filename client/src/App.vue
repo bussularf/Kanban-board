19baa7dabd471301cc2837dc8afa9fb6e55790b0
Fernanda de Jesus <fernandabussular@gmail.com>
2023-11-21 15:57:17 -0300

    /root

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,33 +4,34 @@
     <div class="app" >
       <img alt="Vue logo" src="./assets/logo.png">
       <HelloWorld :users="users" />
+      <login v-if="showLogin"></login>
     </div>
   </div>
 </template>
-
 <script>
 import HelloWorld from './components/HelloWorld.vue';
 import NavBar from './components/NavBar.vue';
+import Login from './components/LoginForm.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
     'nav-bar': NavBar,
+    'login': Login,
   },
   data() {
     return {
       users: [],
+      showLogin: true,
     };
   },
   mounted() {
-    // Faz a solicitação AJAX para obter os usuários do servidor Rails
     this.fetchUsers();
   },
   methods: {
     fetchUsers() {
-      // Substitua a URL com o caminho para o endpoint do Rails que retorna os usuários
-      fetch('http://localhost:8080/users') // Certifique-se de ajustar a porta se necessário
+      fetch('http://localhost:8080/users')
         .then(response => response.json())
         .then(data => {
           this.users = data;
@@ -49,6 +50,7 @@ img {
 }
 
 .app {
-display: flex;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
---
+++ client/src/components/LoginForm.vue
@@ -0,0 +1,44 @@
+<!-- LoginForm.vue -->
+<template>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <label>Email:</label>
+      <input type="email" v-model="email" required>
+
+      <label>Password:</label>
+      <input type="password" v-model="password" required>
+
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    login() {
+      // Enviar dados de login para o backend (usando Axios, por exemplo)
+      this.$axios.post('/login', { email: this.email, password: this.password })
+        .then(response => {
+          // Lógica após login bem-sucedido, como redirecionar para outra página
+          console.log(response.data);
+        })
+        .catch(error => {
+          // Lógica para lidar com falha no login (exibição de mensagem de erro, etc.)
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Estilos específicos para o componente LoginForm, se necessário */
+</style>
